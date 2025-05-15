4836859a37329563bf5f43ad8131eefb551a8640
Matthias Welsch <matthias.welsch@univie.ac.at>
2025-05-15 17:12:45 +0200

    Update documentation

--- src/chemford/__init__.py
+++ src/chemford/__init__.py
@@ -1,9 +1,1 @@
-"""Documentation about chemford."""
-
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Matthias Welsch"
-__email__ = "matthias.welsch@univie.ac.at"
 __version__ = "0.1.0"
