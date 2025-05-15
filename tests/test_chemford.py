6a38483343ea4faa128e20387c51e8c9dbef39c7
Matthias Welsch <matthias.welsch@univie.ac.at>
2025-05-15 17:48:07 +0200

    Utilize only ruff

--- tests/test_chemford.py
+++ tests/test_chemford.py
@@ -1,28 +1,28 @@
-"""Tests for the chemford.my_module module."""
+from chemford.benford.benford import benford_first_digit_distribution
+from chemford.benford.benford import benford_first_two_digit_distribution
+from chemford.benford.benford import benford_n_digit_distribution
 
-from src.chemford.benford.benford import (
-    benford_first_digit_distribution,
-    benford_first_two_digit_distribution,
-    benford_n_digit_distribution,
-)
 
 def test_benford():
-    """Test benford distributions"""
+    """Test benford distributions."""
     first = benford_first_digit_distribution()
     assert isinstance(first, dict), "First digit distribution is no dict"
-    assert len(first) == 9, (
+    expected_value = 9
+    assert len(first) == expected_value, (
         "First digit distribution does not have appropriate number of entries"
     )
 
     first_two = benford_first_two_digit_distribution()
     assert isinstance(first_two, dict), "First digit distribution is no dict"
-    assert len(first_two) == 90, (
+    expected_value = 90
+    assert len(first_two) == expected_value, (
         "First two digits distribution does not have appropriate number of entries"
     )
 
-    for n in range(2, 7): # very long runtimes for high n
+    for n in range(2, 7):  # very long runtimes for high n
         n_th = benford_n_digit_distribution(n)
         assert isinstance(n_th, dict), f"{n}th digit distribution is no dict"
-        assert len(n_th) == 10, (
+        expected_value = 10
+        assert len(n_th) == expected_value, (
             f"{n}th digit distribution does not have appropriate number of entries"
         )
