b560f86be261bdc3c8cd1dbef78b6026d0a9fc40
Matthias Welsch <matthias.welsch@univie.ac.at>
2025-05-15 15:13:41 +0200

    Populate repo with copier with template from wschuell

---
+++ src/chemford/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about chemford."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Matthias Welsch"
+__email__ = "matthias.welsch@univie.ac.at"
+__version__ = "0.1.0"
