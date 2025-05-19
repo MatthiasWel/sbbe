067181c56c76cc2fcb3e56278f5cbc672feef1b7
Matthias Welsch <matthias.welsch@univie.ac.at>
2025-05-19 13:35:33 +0200

    Fix formating

--- tests/test_benford.py
+++ tests/test_benford.py
@@ -1,11 +1,11 @@
-import pandas as pd
 import numpy as np
-
+import pandas as pd
 from chemford.benford.benford import benford_first_digit_distribution
 from chemford.benford.benford import benford_first_two_digit_distribution
 from chemford.benford.benford import benford_n_digit_distribution
+from chemford.benford.benford import has_sufficient_data
+from chemford.benford.benford import has_sufficient_log_scale_coverage
 
-from chemford.benford.benford import has_sufficient_log_scale_coverage, has_sufficient_data
 
 def test_benford_distributions():
     """Test benford distributions."""
@@ -31,20 +31,26 @@ def test_benford_distributions():
             f"{n}th digit distribution does not have appropriate number of entries"
         )
 
+
 def test_sufficiency_checks():
     """Test sufficiency checks"""
     short = np.arange(1, 100, 10)
     long = np.arange(1, 1000, 0.01)
     transforms = (pd.Series, np.array, list)
     for transform in transforms:
-        assert not has_sufficient_data(transform(short)), f"{transform} did not fail with short test data"
-        assert has_sufficient_data(transform(long)), f"{transform} failed with long test data"
+        assert not has_sufficient_data(transform(short)), (
+            f"{transform} did not fail with short test data"
+        )
+        assert has_sufficient_data(transform(long)), (
+            f"{transform} failed with long test data"
+        )
 
-    no_coverage = np.arange(1, 10, 0.01) 
+    no_coverage = np.arange(1, 10, 0.01)
     good_coverage = np.arange(1, 1000, 0.01)
     for transform in transforms:
-        assert not has_sufficient_log_scale_coverage(transform(no_coverage)), f"{transform} did not fail with little coverage"
-        assert has_sufficient_log_scale_coverage(transform(good_coverage)), f"{transform} failed with enough coverage"
-    
-
-    
+        assert not has_sufficient_log_scale_coverage(transform(no_coverage)), (
+            f"{transform} did not fail with little coverage"
+        )
+        assert has_sufficient_log_scale_coverage(transform(good_coverage)), (
+            f"{transform} failed with enough coverage"
+        )
