9fba2d0463a37d6e34013c90b2a95c7cf7d16af1
Matthias Welsch <matthias.welsch@univie.ac.at>
2026-02-10 10:45:56 +0100

    Ignore legacy files

--- scripts/paths.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-PATH_TO_CHEMBL = Path("/data/shared/exchange/uashehab/chembl35/chembl_35.db")
-PATH_TO_SIMULATIONS = Path(
-    "/data/shared/exchange/mwelsch/simulation_chemford/simulation_2025-06-27_18:23:47.csv",
-)
-PATH_TO_PUBCHEM = None
